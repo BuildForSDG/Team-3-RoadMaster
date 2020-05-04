2a62a9f16b53ef0cf2ea67ec1d974e63206fb1a3
tos4christ <gnztrades@gmail.com>
2020-05-04 17:31:43 +0100

    fix: codacy requirements update

--- src/findClosestStation.js
+++ src/findClosestStation.js
@@ -2,7 +2,7 @@
 /* locs would be replaced by the coordinates sent to the server from the client */
 // const locs = { lat: 6.3445645, lng: 3.4533255 };
 /* firestations would be stored and pulled from the database, this is just a mock */
-const { fireStations } = require('../fireStations.json');
+// const { fireStations } = require('../fireStations.json');
 
 /* Function to find the closest firestation to the accident location */
 function findClosestStation(accidentLocation, fireStation) {
@@ -17,25 +17,26 @@ function findClosestStation(accidentLocation, fireStation) {
       min = tempMin;
       closestStation = station;
     }
+    return tempMin;
   }, {});
   return closestStation;
 }
 
 /* Function to find the optimal route between the closest firestation and the accident location */
-function optimalRoute() {
+// function optimalRoute() {
 
-}
+// }
 
 /* This would be used as a controller for the route */
-const mapsApi = (req, res, next) => {
-  // Coordinates of the accident location
-  const { accidentLocation } = req.body;
-  const closestStation = findClosestStation(accidentLocation, fireStations);
-  res.json({
-    closestStation,
-    optimalRouteCoords: []
-  });
-};
+// const mapsApi = (req, res) => {
+//   // Coordinates of the accident location
+//   const { accidentLocation } = req.body;
+//   const closestStation = findClosestStation(accidentLocation, fireStations);
+//   res.json({
+//     closestStation,
+//     optimalRouteCoords: []
+//   });
+// };
 
 /* This controller would be exported to the routes module to be used in the maps logic */
 export default findClosestStation;
