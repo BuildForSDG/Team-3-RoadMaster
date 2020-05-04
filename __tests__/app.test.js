ab545783fcf69eb603696a93d8b83897a1fe297a
tos4christ <gnztrades@gmail.com>
2020-05-04 17:09:31 +0100

    feat: add unit test for this feature in jest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,6 @@
-import app from '../src/index';
+import app from '../src/app';
+import findClosestStation from '../src/findClosestStation';
+import fireStations from '../fireStations.json';
 
 describe('app module', () => {
   test('it exists', async () => {
@@ -9,5 +11,12 @@ describe('app module', () => {
     const result = await app();
     const sdgPos = (result || '').indexOf('SDG');
     expect(sdgPos).toBeGreaterThanOrEqual(0);
+  });
+
+  test('it finds closest fire station', () => {
+    /* user location sent from the client via geolocation api */
+    const userLocation = { lat: 6.3445645, lng: 3.4533255 };
+    const nearestStation = findClosestStation(userLocation, fireStations.fireStations);
+    expect(...nearestStation).toEqual('Lekki');
   });
 });
--- src/findClosestStation.js
+++ src/findClosestStation.js
@@ -1,21 +1,14 @@
 /* eslint-disable linebreak-style */
 /* locs would be replaced by the coordinates sent to the server from the client */
-const locs = { lat: 6.3445645, lng: 3.4533255 };
+// const locs = { lat: 6.3445645, lng: 3.4533255 };
 /* firestations would be stored and pulled from the database, this is just a mock */
-const fireStations = [
-  { Lekki: { lat: 6.4355376, lng: 3.4829765 } },
-  { Ajah: { lat: 6.4682876, lng: 3.5719866 } },
-  { Ikeja: { lat: 6.6004382, lng: 3.3513756 } },
-  { Ikorodu: { lat: 6.6144343, lng: 3.5094988 } },
-  { Epe: { lat: 6.5854286, lng: 3.9566465 } },
-  { Sangotedo: { lat: 6.4727703, lng: 3.6309526 } }
-];
+const { fireStations } = require('../fireStations.json');
 
 /* Function to find the closest firestation to the accident location */
-function findClosestStation(accidentLocation) {
+function findClosestStation(accidentLocation, fireStation) {
   let closestStation;
   let min = Infinity;
-  fireStations.reduce((init, next) => {
+  fireStation.reduce((init, next) => {
     const station = Object.keys(next);
     const latDiff = Math.abs(accidentLocation.lat - next[station].lat);
     const lngDiff = Math.abs(accidentLocation.lng - next[station].lng);
@@ -33,19 +26,16 @@ function optimalRoute() {
 
 }
 
+/* This would be used as a controller for the route */
 const mapsApi = (req, res, next) => {
   // Coordinates of the accident location
   const { accidentLocation } = req.body;
-  const closestStation = findClosestStation(accidentLocation);
+  const closestStation = findClosestStation(accidentLocation, fireStations);
   res.json({
     closestStation,
     optimalRouteCoords: []
   });
 };
 
-/* This is to manually test the function and see that it works */
-const cs = findClosestStation(locs);
-console.log(cs);
-
 /* This controller would be exported to the routes module to be used in the maps logic */
-// export default mapsApi;
+export default findClosestStation;
