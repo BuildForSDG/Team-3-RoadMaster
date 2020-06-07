109e56153abae0138b17bd569e5ac94a645e22b6
tos4christ <gnztrades@gmail.com>
2020-06-07 22:37:45 +0100

    lint code for codacy

--- src/utility/socketioConnection.js
+++ src/utility/socketioConnection.js
@@ -1,9 +1,8 @@
 import openSocket from 'socket.io-client';
- // start the client socket
-  // Local Server
- // const socket = openSocket('http://localhost:5000/');
-    // Heroku Server
- const socket = openSocket('https://covid-19-tos4christ.herokuapp.com/'); 
+// start the client socket
+// Local Server
+// const socket = openSocket('http://localhost:5000/');
+// Heroku Server
+const socket = openSocket('https://covid-19-tos4christ.herokuapp.com/');
 
- export default socket;
- 
+export default socket;
