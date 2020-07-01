b377f31e00a3a2dd0509e7e94bab91a70eaeb77a
tos4christ <gnztrades@gmail.com>
2020-07-01 14:39:40 +0100

    add upload forms to user for reporting images

--- src/pages/DashboardHomeUser.js
+++ src/pages/DashboardHomeUser.js
@@ -7,6 +7,8 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 
 const DashboardHomeUser = () => {
   let [modalMessage, setModal] = useState("");
+  let [imageReport, setImage] = useState("");
+  let [imageStyle, setStyle] = useState("hidden");
 
   // function to display when the reply gets back
   const reply =  function(data) {
@@ -67,6 +69,19 @@ const DashboardHomeUser = () => {
       handleError(err);
     }
   }
+  function submitReport() {
+
+  }
+  function showImage(e) {
+    if(e.target.files[0]) {
+      const path = URL.createObjectURL(e.target.files[0]);
+      setImage(path);
+      setStyle("")
+    } else if(!e.target.files[0]) {
+      setImage("");
+      setStyle("hidden");
+    }
+  }
   const sosStyle = {
     borderRadius: "50%",
     width: "100px",
@@ -77,40 +92,44 @@ const DashboardHomeUser = () => {
   const body = (
     <div className="container-fluid">
       <div className='row' style={{ height: "1vh" }}>
-        <button style={{display: "none"}} type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+        <button style={{display: "none"}} type="button" className="btn btn-primary" data-toggle="modal" data-target="#myModal">
           Open modal
         </button>
-        <div class="modal" id="myModal">
-          <div class="modal-dialog" >
-            <div class="modal-content">
-
-              <div class="modal-header">
-                <h4 class="modal-title">Dear user</h4>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <div className="modal" id="myModal">
+          <div className="modal-dialog" >
+            <div className="modal-content">
+              <div className="modal-header">
+                <h4 className="modal-title">Dear user</h4>
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
               </div>
-
-              <div class="modal-body">
+              <div className="modal-body">
                 {modalMessage}
               </div>
-
-              <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
               </div>
-
             </div>
           </div>
         </div>
         <div className='col text-center text-success mt-5 pt-5 mb-0 pb-0'>
           <h2> Request for an emergency service below</h2>
-          <form className="row">
-            <div className="col-6 ml-auto mr-auto pt-3 form-group">
-            <label for="description">Describe the help you need below or just press button for emergency and help would come</label>
-              <input className="form-control" type="text" id="description" placeholder="Describe your situation"></input>
-            </div>
+          <form className="row" encType="multipart/form-data" name="reports" onSubmit={submitReport}>
+
+              <div className="col-6 ml-auto mr-auto pt-3 form-group">
+                <label htmlFor="description">Describe the help you need below or just press button for emergency and help would come</label>
+                <input className="form-control" type="text" id="description" placeholder="Describe your situation"></input>
+
+                <label> Upload a picture of the scene</label><br/>
+                <input type='file' name='imageReport' onChange={showImage} accept="image/jpg" /><br />
+                <img src={imageReport} style={{display: imageStyle, height: "10vh"}} alt=""/>
+                <button className='button ml-3 mt-3'> Upload your image report </button>
+              </div>
+
+
           </form>
         </div>
       </div>
-      <div style={{ height: "80vh" }} className="row">
+      <div style={{ height: "80vh", marginTop: '150px' }} className="row">
         <button
           type="button"
           className="my-auto"
